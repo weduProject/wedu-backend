7cb507fa99b1f0834998e97cd676bd96fd0aa283
trymimi <applemimicom@gmail.com>
2026-08-05 15:28:51 +0900

    fix: 운영 프로파일 Swagger 로그 설정 분리 #49

--- src/test/java/com/wedu/global/config/ProfileConfigurationTest.java
+++ src/test/java/com/wedu/global/config/ProfileConfigurationTest.java
@@ -4,24 +4,10 @@ import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Properties;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ValueSource;
 import org.springframework.beans.factory.config.YamlPropertiesFactoryBean;
 import org.springframework.core.io.ClassPathResource;
 
 class ProfileConfigurationTest {
-
-    @ParameterizedTest
-    @ValueSource(strings = {"application-local.yml", "application-dev.yml"})
-    void developmentProfilesEnableSwaggerAndDebugLogging(String resourceName) {
-        Properties properties = loadYaml(resourceName);
-
-        assertThat(properties)
-                .containsEntry("springdoc.api-docs.enabled", true)
-                .containsEntry("springdoc.swagger-ui.enabled", true)
-                .containsEntry("logging.level.com.wedu", "DEBUG")
-                .containsEntry("logging.level.org.hibernate.SQL", "DEBUG");
-    }
 
     @Test
     void prodProfileDisablesSwaggerAndUsesNonDebugLogging() {
